ff1057a53d71de231a1fcae90c9ef19af1734976
mk$weety <94924014+Mucoloff@users.noreply.github.com>
2025-01-29 23:22:24 +0100

    Update ChancheProcessor.java

--- src/main/java/dev/kirby/processors/ChancheProcessor.java
+++ src/main/java/dev/kirby/processors/ChancheProcessor.java
@@ -2,9 +2,16 @@ package dev.kirby.processors:
 
 import dev.kirby.processors.*;
 
+@Getter
+@Setter
 public class ChancheProcessor extends BountyProcessor {
 
+     private double percentage = 0;
 
+  public void clearChanche() {
+       
+    
+  }
 
 
 }
